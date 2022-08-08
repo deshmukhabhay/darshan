f968fc2cf67cda77e1d6afadd62f4964ad3e0815
Abhay Deshmukh <abhaydeshmukh@Abhays-MacBook-Air.local>
2022-08-08 20:46:28 +0530

    first commit

---
+++ src/pages/homepage/HomePage.js
@@ -0,0 +1,326 @@
+import React from "react";
+import "../../assets/css/HomePage.css";
+import Darshan from "../../assets/images/DharoharDarshan.png";
+import Vector from "../../assets/images/Vector.png";
+import Group from "../../assets/images/Group.png";
+import Group1 from "../../assets/images/Group1.png";
+import Vellore from "../../assets/images/vellore.png";
+
+export default function HomePage() {
+  return (
+    <div>
+      <div className={"container"}></div>
+      <div className={"container2"}></div>
+      <div className={"image_carousel"}></div>
+      <div className={"image"}></div>
+      <div className={"frame_39615"}>
+        <div className={"rect_4540"}></div>
+      </div>
+      <div className={"rect_4542"}></div>
+      <div className={"rect_4538"}></div>
+      {/* <div className={"ellipse_153"}></div> */}
+      <img className={"DharoharDarshan"} src={Darshan} />
+      <span className={"DharoharDarshanText"}>Dharohar Darshan</span>
+      <span className={"HomeText"}>Home</span>
+      <span className={"MapViewText"}>Map View</span>
+      <span className={"AboutText"}>About</span>
+      <span className={"DiscoverText"}>Discover</span>
+      <span className={"BlogText"}>Blog</span>
+      <div className={"LanguageDropdown"}>
+        <span className={"EnglishText"}>English</span>
+        <span className={"LanguageVector"}>
+          <img className={"Vector"} src={Vector} />
+        </span>
+      </div>
+      <div className={"group_39642"}>
+        {/* <div className={"group_39641"}> */}
+        <img className={"group0"} src={Group} />
+        <img className={"group1"} src={Group} />
+        <img className={"group2"} src={Group} />
+        <img className={"group3"} src={Group} />
+        <img className={"group4"} src={Group} />
+        <img className={"group5"} src={Group} />
+        <img className={"group6"} src={Group} />
+        <img className={"group7"} src={Group} />
+        <img className={"group8"} src={Group} />
+        <img className={"group9"} src={Group} />
+        {/* </div> */}
+        {/* <div className={"group_39640"}> */}
+        <img className={"group11"} src={Group} />
+        <img className={"group12"} src={Group} />
+        <img className={"group13"} src={Group} />
+        <img className={"group14"} src={Group} />
+        <img className={"group15"} src={Group} />
+        <img className={"group16"} src={Group} />
+        <img className={"group17"} src={Group} />
+        <img className={"group18"} src={Group} />
+        <img className={"group19"} src={Group} />
+        {/* </div> */}
+        {/* <div className={"group_39639"}> */}
+        {/* <img className={"group31"} src={Group}/> */}
+        <img className={"group32"} src={Group} />
+        <img className={"group33"} src={Group} />
+        <img className={"group34"} src={Group} />
+        <img className={"group35"} src={Group} />
+        <img className={"group36"} src={Group} />
+        <img className={"group37"} src={Group} />
+        <img className={"group38"} src={Group} />
+        <img className={"group39"} src={Group} />
+        <img className={"group40"} src={Group} />
+        <img className={"group41"} src={Group} />
+        <img className={"group42"} src={Group} />
+        {/* </div> */}
+      </div>
+      <div className={"carousel_text"}>
+        Our Country is defined by our Heritage
+      </div>
+      <div className={"group_39643"}>
+        <img className={"group51"} src={Group} />
+        <img className={"group52"} src={Group} />
+        <img className={"group53"} src={Group} />
+        <img className={"group54"} src={Group} />
+        <img className={"group55"} src={Group} />
+        <img className={"group56"} src={Group} />
+        <img className={"group57"} src={Group} />
+        <img className={"group58"} src={Group} />
+        <img className={"group59"} src={Group} />
+        <img className={"group60"} src={Group} />
+      </div>
+      <div className={"what_is_dharohar_text"}>What is Dharohar Darshan</div>
+      <div className={"group_39632"}>
+        <img className={"group61"} src={Group} />
+        <img className={"group62"} src={Group} />
+        <img className={"group63"} src={Group} />
+        <img className={"group64"} src={Group} />
+        <img className={"group65"} src={Group} />
+        <img className={"group66"} src={Group} />
+        <img className={"group67"} src={Group} />
+        <img className={"group68"} src={Group} />
+        <img className={"group69"} src={Group} />
+        <img className={"group70"} src={Group} />
+      </div>
+
+      <div className={"todays_darshan_text"}>
+        See what happened today in History{" "}
+      </div>
+      <div className={"dtext2"}>
+        The rich heritage of India, one of the world's oldest civilizations, is
+        an all-embracing confluence of religions, traditions and customs. The
+        highlights of Indian heritage lie in the treasure of its art,
+        architecture, classical dance, music, flora and fauna
+      </div>
+      <div className={"dtext3"}>
+        The Indian culture is described as 'Sa Prathama Sanskrati Vishvavara' -
+        the first and the supreme culture in the world. It is honored as a
+        divine culture. The core of Indian culture, as shaped by the revered
+        rishis, revolves around the principle of 'Vasudhaiva Kutumbakam' -
+        treating the entire world as our own family, which reflects universality
+        of serene love, altruism, sharing of responsibilities and caring for all
+        beings
+      </div>
+
+      <div className={"rect_4541"}></div>
+      <div className={"rect_39360"}></div>
+      <div className={"rect_39530"}>
+        <div className={"counter_text"}>300+</div>
+        <div className={"sites_text"}>Cultural sites</div>
+      </div>
+      <div className={"rect_39531"}>
+        <div className={"counter_text1"}>200+</div>
+        <div className={"sites_text1"}>Natural Sites</div>
+      </div>
+      <div className={"rect_39532"}>
+        <div className={"counter_text2"}>150+</div>
+        <div className={"sites_text2"}>Protected Sites</div>
+      </div>
+      <div className={"rect_39533"}>
+        <div className={"counter_text3"}>100+</div>
+        <div className={"sites_text3"}>Temples</div>
+      </div>
+      <div className={"visit_now"}>
+        <div className={"visit_now_text"}>Visit now</div>
+      </div>
+      <div className={"group_39380"}>
+        <div className={"group_39645"}>
+          <img className={"group61"} src={Group1} />
+          <img className={"group62"} src={Group1} />
+          <img className={"group63"} src={Group1} />
+          <img className={"group64"} src={Group1} />
+          <img className={"group65"} src={Group1} />
+          <img className={"group66"} src={Group1} />
+          <img className={"group67"} src={Group1} />
+          <img className={"group68"} src={Group1} />
+          <img className={"group69"} src={Group1} />
+          <img className={"group70"} src={Group1} />
+          <img className={"group81"} src={Group1} />
+          <img className={"group82"} src={Group1} />
+        </div>
+        <div className={"todays_darshan"}>Today’s Darshan</div>
+        <div className={"group_39644"}>
+          <img className={"group91"} src={Group1} />
+          <img className={"group92"} src={Group1} />
+          <img className={"group93"} src={Group1} />
+          <img className={"group94"} src={Group1} />
+          <img className={"group95"} src={Group1} />
+          <img className={"group96"} src={Group1} />
+          <img className={"group97"} src={Group1} />
+          <img className={"group98"} src={Group1} />
+          <img className={"group99"} src={Group1} />
+          <img className={"group100"} src={Group1} />
+          <img className={"group101"} src={Group1} />
+          <img className={"group102"} src={Group1} />
+        </div>
+      </div>
+      <div className={"todays_darshan_parent"}>
+        <div className={"frame_39600"}>
+          <div
+            style={{ backgroundImage: `url("assets/images/vellore.png")` }}
+            className={"rectange_30"}
+          ></div>
+          <div className={"vellore_text"}>Vellore Fort</div>
+          <div className={"frame_39581"}>
+            <span className={"read_more"}>Read more</span>
+          </div>
+        </div>
+        <div className={"frame_39600"}>
+          <div
+            style={{ backgroundImage: `url("assets/images/shambutemple.png")` }}
+            className={"rectange_30"}
+          ></div>
+          <div className={"vellore_text"}>Vellore Fort</div>
+          <div className={"frame_39581"}>
+            <span className={"read_more"}>Read more</span>
+          </div>
+        </div>
+        <div className={"frame_39600"}>
+          <div
+            style={{
+              backgroundImage: `url("assets/images/Cholesvaratemple.png")`,
+            }}
+            className={"rectange_30"}
+          ></div>
+          <div className={"vellore_text"}>Vellore Fort</div>
+          <div className={"frame_39581"}>
+            <span className={"read_more"}>Read more</span>
+          </div>
+        </div>
+        <div className={"frame_39600"}>
+          <div
+            style={{
+              backgroundImage: `url("assets/images/SubramanyaswamyTemple.png")`,
+            }}
+            className={"rectange_30"}
+          ></div>
+          <div className={"vellore_text"}>Vellore Fort</div>
+          <div className={"frame_39581"}>
+            <span className={"read_more"}>Read more</span>
+          </div>
+        </div>
+      </div>
+      <div className={"group_39860"}>
+        <div className={"group_39647"}>
+          <img className={"group61"} src={Group1} />
+          <img className={"group62"} src={Group1} />
+          <img className={"group63"} src={Group1} />
+          <img className={"group64"} src={Group1} />
+          <img className={"group65"} src={Group1} />
+          <img className={"group66"} src={Group1} />
+          <img className={"group67"} src={Group1} />
+          <img className={"group68"} src={Group1} />
+          <img className={"group69"} src={Group1} />
+          <img className={"group70"} src={Group1} />
+          <img className={"group81"} src={Group1} />
+          <img className={"group82"} src={Group1} />
+        </div>
+        <div className={"todays_darshan2"}>10 Places to Visit</div>
+        <div className={"group_39646"}>
+          <img className={"group91"} src={Group1} />
+          <img className={"group92"} src={Group1} />
+          <img className={"group93"} src={Group1} />
+          <img className={"group94"} src={Group1} />
+          <img className={"group95"} src={Group1} />
+          <img className={"group96"} src={Group1} />
+          <img className={"group97"} src={Group1} />
+          <img className={"group98"} src={Group1} />
+          <img className={"group99"} src={Group1} />
+          <img className={"group100"} src={Group1} />
+          <img className={"group101"} src={Group1} />
+          <img className={"group102"} src={Group1} />
+        </div>
+        <div className={"todays_darshan_text2"}>
+          See what happened today in History{" "}
+        </div>
+        <div className={"todays_darshan_parent2"}>
+          <div className={"frame_39600"}>
+            <div
+              style={{ backgroundImage: `url("assets/images/vellore.png")` }}
+              className={"rectange_30"}
+            ></div>
+            <div className={"vellore_text"}>Vellore Fort</div>
+            <div className={"frame_39581"}>
+              <span className={"read_more"}>Read more</span>
+            </div>
+          </div>
+          <div className={"frame_39600"}>
+            <div
+              style={{
+                backgroundImage: `url("assets/images/shambutemple.png")`,
+              }}
+              className={"rectange_30"}
+            ></div>
+            <div className={"vellore_text"}>Vellore Fort</div>
+            <div className={"frame_39581"}>
+              <span className={"read_more"}>Read more</span>
+            </div>
+          </div>
+          <div className={"frame_39600"}>
+            <div
+              style={{
+                backgroundImage: `url("assets/images/Cholesvaratemple.png")`,
+              }}
+              className={"rectange_30"}
+            ></div>
+            <div className={"vellore_text"}>Vellore Fort</div>
+            <div className={"frame_39581"}>
+              <span className={"read_more"}>Read more</span>
+            </div>
+          </div>
+          <div className={"frame_39600"}>
+            <div
+              style={{
+                backgroundImage: `url("assets/images/SubramanyaswamyTemple.png")`,
+              }}
+              className={"rectange_30"}
+            ></div>
+            <div className={"vellore_text"}>Vellore Fort</div>
+            <div className={"frame_39581"}>
+              <span className={"read_more"}>Read more</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={"footer"}>
+        <div className={"govt_logos"}>
+          <div>
+            <div className={"partner1"}></div>
+            <div className={"partner2"}></div>
+            <div className={"partner3"}></div>
+            <div className={"partner4"}></div>
+          </div>
+          <div className={"group_39629"}></div>
+          <div className={"group_39633"}></div>
+        </div>
+        <div className={"rect_4539"}></div>
+
+        <div className={"group_39661"}></div>
+        <div className={"footer_link1"}></div>
+        <div className={"footer_link2"}></div>
+        <div className={"footer_content"}></div>
+        <div className={"footer_left"}>Copyright © 2022 ASI</div>
+        <div className={"footer_right"}>
+          All Rights Reserved | Terms and Conditions | Privacy Policy
+        </div>
+      </div>
+    </div>
+  );
+}
