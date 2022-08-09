2990334d2a63bc3d0a7ef1879ac06021e211554b
Atul Ubarhande <atul.ubarhande@promobitech.com>
2022-08-09 21:19:42 +0530

    added 360 image viewer

--- src/pages/LocationPage/LocationPage.js
+++ src/pages/LocationPage/LocationPage.js
@@ -4,15 +4,34 @@ import Header2 from "../../components/Header2";
 
 import "../../assets/css/HomePage.css";
 import { BiChevronLeft } from "react-icons/bi";
-
-
+import ReactPannellum, { getConfig } from "react-pannellum";
+import Img from '../../assets/images/somnath-temple-banner.jpeg'
 export default function MapPage() {
+
+  const click = () => {
+      console.log(getConfig());
+  }
+
+  const config = {
+    autoRotate: -2,
+    autoLoad: true,
+    showZoomCtrl: true
+  };
+
+
   return (
     <div>
       <div className={"container3"}></div>
       <Header2 />
       <div className={"banner_text"}>Somnath Temple</div>
       <BiChevronLeft className={"vector"}/>
+      <ReactPannellum
+          id="1"
+          sceneId="firstScene"
+          imageSource={Img}
+          config={config}
+        />
+        <div onClick={() => click()}>Click me</div>
       <Footer />
     </div>
   );
