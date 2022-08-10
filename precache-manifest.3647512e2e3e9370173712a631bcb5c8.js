fc06ec2fa261879da4344dee932ca7c207eec619
Atul Ubarhande <atul.ubarhande@promobitech.com>
2022-08-10 12:01:08 +0530

    Updates

---
+++ precache-manifest.3647512e2e3e9370173712a631bcb5c8.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e76b65b47a71a7773f7",
+    "url": "/darshan/static/css/main.1552febc.chunk.css"
+  },
+  {
+    "revision": "3e76b65b47a71a7773f7",
+    "url": "/darshan/static/js/main.66757527.chunk.js"
+  },
+  {
+    "revision": "7060f18d5041fa9274df",
+    "url": "/darshan/static/js/runtime~main.79aa798d.js"
+  },
+  {
+    "revision": "67623296100ba917c1fb",
+    "url": "/darshan/static/js/2.92159578.chunk.js"
+  },
+  {
+    "revision": "114a46de5c05ca60e778",
+    "url": "/darshan/static/js/3.c7b6313b.chunk.js"
+  },
+  {
+    "revision": "6c9f9b2541e1e0b888b6daed201596e9",
+    "url": "/darshan/static/media/DharoharDarshan.6c9f9b25.png"
+  },
+  {
+    "revision": "b9320cd413db811cdf02d8a071575047",
+    "url": "/darshan/static/media/1.b9320cd4.png"
+  },
+  {
+    "revision": "93e7d10b75c81568f3c72e426054d17a",
+    "url": "/darshan/static/media/2.93e7d10b.png"
+  },
+  {
+    "revision": "5fb3913cb6800dbcfc0483ac3f22ec35",
+    "url": "/darshan/static/media/Group39629.5fb3913c.png"
+  },
+  {
+    "revision": "fcb27a57514fca2426174e894ff6c83f",
+    "url": "/darshan/static/media/homepage.fcb27a57.png"
+  },
+  {
+    "revision": "f58a44c5cab4eb9ca723b14d6121f899",
+    "url": "/darshan/static/media/Dharohar_Darshan_website_1.f58a44c5.png"
+  },
+  {
+    "revision": "4ef3405a47d7c672f762c0b0ee67873a",
+    "url": "/darshan/static/media/somnath-temple-banner.4ef3405a.jpeg"
+  },
+  {
+    "revision": "82f4596511e09a497e8909e362ca57ba",
+    "url": "/darshan/static/media/pexels-pixabay-358482.82f45965.jpg"
+  },
+  {
+    "revision": "bc7d89334ae61a29c8a856725a0a8c96",
+    "url": "/darshan/static/media/India.bc7d8933.png"
+  },
+  {
+    "revision": "35f74373b9b413b1c3642a3cbb407adc",
+    "url": "/darshan/static/media/Rectangle26.35f74373.png"
+  },
+  {
+    "revision": "4dacddf4eb035acba6bb6d127ccf69a3",
+    "url": "/darshan/static/media/Rectangle28.4dacddf4.png"
+  },
+  {
+    "revision": "7f7f65f6db63574f86088d0cffeda155",
+    "url": "/darshan/static/media/Rectangle29.7f7f65f6.png"
+  },
+  {
+    "revision": "066f6ca3a6b75fe9f7ffe6729d9aae33",
+    "url": "/darshan/static/media/Rectangle27.066f6ca3.png"
+  },
+  {
+    "revision": "c501e205e31d1c6899ad464fd3ca9e7f",
+    "url": "/darshan/static/media/Rectangle35.c501e205.png"
+  },
+  {
+    "revision": "c6c168c965375b30b88f288474c984a5",
+    "url": "/darshan/static/media/Rectangle32.c6c168c9.png"
+  },
+  {
+    "revision": "abd2cca5a6e7e825f0f51b603747c5a4",
+    "url": "/darshan/static/media/Rectangle4487.abd2cca5.png"
+  },
+  {
+    "revision": "d2c255d8633d0b7e9bd4508cb079938e",
+    "url": "/darshan/static/media/site.d2c255d8.jpeg"
+  },
+  {
+    "revision": "c7857008a9c0f7e3f32323261293c9f4",
+    "url": "/darshan/index.html"
+  }
+];
